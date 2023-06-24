453350eb6135b8a632dc83090a1c096f00d077bd
varomnrg <varomnrg@gmail.com>
2023-06-24 15:41:16 +0700

    Added threads table

---
+++ migrations/1687593063618_create-table-threads.js
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+    pgm.createTable("threads", {
+        id: {
+            type: "VARCHAR(50)",
+            primaryKey: true,
+        },
+        title: {
+            type: "VARCHAR(50)",
+            notNull: true,
+        },
+        body: {
+            type: "TEXT",
+            notNull: true,
+        },
+        owner: {
+            type: "VARCHAR(50)",
+            notNull: true,
+            references: '"users"',
+            onDelete: "cascade",
+        },
+        date: {
+            type: "TEXT",
+            notNull: true,
+        },
+    });
+};
+
+exports.down = (pgm) => {
+    pgm.dropTable("threads");
+};
+
